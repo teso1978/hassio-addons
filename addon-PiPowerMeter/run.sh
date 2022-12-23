8c49956fc7f2c012de9b490e168ad2b94304c808
Hajdu István <teso1978@gmail.com>
2022-12-23 17:37:11 +0100

    update run.sh

--- addon-PiPowerMeter/run.sh
+++ addon-PiPowerMeter/run.sh
@@ -9,4 +9,5 @@ cd /app
 node -v
 npm -v
 npm install
-forever start --workingDir /app -a -o /dev/null -e /nodejs.err.log --killSignal=SIGTERM /app/server.js
+node server.js
+#forever start --workingDir /app -a -o /dev/null -e /nodejs.err.log --killSignal=SIGTERM /app/server.js
